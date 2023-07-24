1a50ca867b0883bfb47a77b51cd01313c1730db1
Zetan Li <zetanli@C02FX268ML85.local>
2023-07-23 21:23:30 -0700

    fixed setting os.

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
-                 'travere-dd6090e37b3c.herokuapp.com/',
+                 'travere-dd6090e37b3c.herokuapp.com',
                  ]
 
 
